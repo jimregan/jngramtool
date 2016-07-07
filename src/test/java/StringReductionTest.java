2cea840c352a352732fa3384ba4bfa30ec2c3176
Jim O'Regan <joregan@gmail.com>
2016-07-07 14:03:14 +0100

    basic test

--- src/test/java/StringReductionTest.java
+++ src/test/java/StringReductionTest.java
@@ -1,14 +1,25 @@
 import org.junit.Test;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by Abair on 07/07/2016.
- */
 public class StringReductionTest {
+    Map<String, Integer> m = new TreeMap<String, Integer>();
+
+    Map<String, Integer> e1 = new TreeMap<String, Integer>();
+    public StringReductionTest() {
+        m.put("this is a test", 6);
+        m.put("is a test", 6);
+        m.put("this is a", 6);
+
+        e1.put("this is a test", 6);
+    }
     @Test
     public void reduce() throws Exception {
-
+        Map<String, Integer> res = StringReduction.reduce(m, 5);
+        assert(res.toString().equals(e1.toString()));
     }
 
 }
