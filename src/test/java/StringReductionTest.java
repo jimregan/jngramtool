383ac800d384ed6bbca28b089454b3449bb3fd3a
Jim O'Regan <joregan@gmail.com>
2016-07-07 14:07:45 +0100

    add commented test: reduce1 works in a weird way, I think

--- src/test/java/StringReductionTest.java
+++ src/test/java/StringReductionTest.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2016 Jim O'Regan <jaoregan@tcd.ie>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 import org.junit.Test;
 
 import java.util.Map;
@@ -6,20 +27,30 @@ import java.util.TreeMap;
 import static org.junit.Assert.*;
 
 public class StringReductionTest {
-    Map<String, Integer> m = new TreeMap<String, Integer>();
-
+    Map<String, Integer> m1 = new TreeMap<String, Integer>();
     Map<String, Integer> e1 = new TreeMap<String, Integer>();
-    public StringReductionTest() {
-        m.put("this is a test", 6);
-        m.put("is a test", 6);
-        m.put("this is a", 6);
+    Map<String, Integer> m2 = new TreeMap<String, Integer>();
+    Map<String, Integer> e2 = new TreeMap<String, Integer>();
 
+    public StringReductionTest() {
+        m1.put("this is a test", 6);
+        m1.put("is a test", 6);
+        m1.put("this is a", 6);
         e1.put("this is a test", 6);
+
+        m2.put("this is a test", 6);
+        m2.put("is a test", 8);
+        m2.put("this is a", 2);
+        e2.put("this is a test", 6);
+        e2.put("is a test", 2);
     }
     @Test
     public void reduce() throws Exception {
-        Map<String, Integer> res = StringReduction.reduce(m, 5);
-        assert(res.toString().equals(e1.toString()));
+        Map<String, Integer> res1 = StringReduction.reduce(m1, 5);
+        assert(res1.toString().equals(e1.toString()));
+        //reduce1's algorithm seems to not reduce in the way I expect
+        //Map<String, Integer> res2 = StringReduction.reduce(m2, 5);
+        //assert(res2.toString().equals(e2.toString()));
     }
 
 }
