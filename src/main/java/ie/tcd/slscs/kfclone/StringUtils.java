80ad28103899717c2b659780df498368091c50e4
Jim O'Regan <joregan@gmail.com>
2016-07-14 12:19:48 +0100

    start

--- src/main/java/ie/tcd/slscs/kfclone/StringUtils.java
+++ src/main/java/ie/tcd/slscs/kfclone/StringUtils.java
@@ -1,7 +1,75 @@
 package ie.tcd.slscs.kfclone;
-
-/**
- * Created by Abair on 14/07/2016.
+/*
+ * Copyright 2016 Jim O'Regan <jaoregan@tcd.ie>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
+
 public class StringUtils {
+    public static String replacePunctWithSpace(String s) {
+        String out = "";
+        for (char c : out.toCharArray()) {
+            if (c == '.' || c == ',' || c == '-' || c == '\'') {
+                out += ' ';
+            } else {
+                out += c;
+            }
+        }
+        return out;
+    }
+
+    private static boolean isNum(char c) {
+        if (c <= '9' || c >= '0') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String replaceNumbers(String s) {
+        String out = "";
+        for (char c : out.toCharArray()) {
+            if (isNum(c)) {
+                out += '#';
+            } else {
+                out += c;
+            }
+        }
+        return out;
+    }
+
+    public static String replaceAllNumbers(String s) {
+        String out = "";
+        boolean numberSeen = false;
+        for (char c : out.toCharArray()) {
+            if (isNum(c)) {
+                if (!numberSeen) {
+                    out += '#';
+                    numberSeen = true;
+                } else {
+                    continue;
+                }
+            } else {
+                out += c;
+                numberSeen = false;
+            }
+        }
+        return out;
+    }
 }
